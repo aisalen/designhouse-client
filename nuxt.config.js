d66a253df985f2cdb944876a1f2a92acba235d11
junji <aisalen@gmail.com>
2020-06-23 19:25:15 +0500

    End of section 12

---
+++ nuxt.config.js
@@ -0,0 +1,94 @@
+
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    // 12.93
+    script: [
+      { src: "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"},
+      { src: "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.bundle.min.js"},
+      { src: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/js/all.min.js"},
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+    // 12.94
+    '@/assets/scss/main.scss'
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    // 12.97
+    '~plugins/vform'
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+    // 12.95
+    '@nuxtjs/router',
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    'bootstrap-vue/nuxt',
+    // 12.96
+    '@nuxtjs/auth',
+
+    '@nuxtjs/axios',
+    '@nuxtjs/dotenv',
+  ],
+  // 12.96
+  auth: {
+    strategies: {
+      local: {
+        endpoints: {
+          login: { url: '/login', method: 'post', propertyName: 'token' },
+          logout: { url: '/logout', method: 'post' },
+          user: { url: '/me', method: 'get', propertyName: 'data' }
+        },
+        // tokenRequired: true,
+        // tokenType: 'bearer',
+        // globalToken: true,
+        // autoFetchUser: true
+      }
+    }
+  },
+  /*
+  ** Axios module configuration
+  ** See https://axios.nuxtjs.org/options
+  */
+  axios: {
+    // 12.92
+    baseUrl: process.env.API_URL
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <i class="fas fa-user"></i>
+    <!-- 12.96 -->
+    <!-- {{ $auth.loggedIn }} -->
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
